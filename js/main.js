b9f4862ab54b82eb24b0db068d933c66b5fe1fb7
Jerry Lee <hyunsuk.lee1@gmail.com>
2023-02-17 03:10:43 -0500

    sounds in tic tac toe

--- js/main.js
+++ js/main.js
@@ -1,23 +1,42 @@
 console.log('Hello')
 
-/*----- constants -----*/
+/*---------------------- constants ----------------*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
 
-/*----- app's state (variables) -----*/
+/*----------------- app's state (variables) ---------------*/
 let board;
 
 let turn = 'X';
 
+let win;
 
+let scoreboard = {x : 0, o :0};
 
-/*----- cached element references -----*/
+const click = new Audio("sounds/click.wav"); //sounds everytime we click 
+
+/*--------------- cached element references --------------*/
 const squares = Array.from(document.querySelectorAll('#board div')); 
     //queryselectorall () allows us to find the element with the id of .board and selecting all the div children of that element(grabs a secion of a div)
 
-/*----- event listeners -----*/
+const messages = document.querySelector('h2'); //selects the whole h2 section from document 
+
+
+/*-------------------- event listeners ----------------------*/
 document.getElementById('board').addEventListener('click', handleTurn);
 
-/*----- functions -----*/
+document.getElementById('reset-button').addEventListener('click', init);
+
+/*----- -----------------functions -------------------*/
 function init() { //init function to check if squares are present 
     board = [
     '', '', '',
@@ -34,6 +53,8 @@ function render() {
         //this moves the value of the board item into the squares[idx]
         squares[index].textContent = mark;
         });
+
+        messages.textContent = win === 'T' ? `TIE GAME! ` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
         };
 
     init();   
@@ -44,6 +65,43 @@ function handleTurn(event) {
         });
         
         board[idx] = turn;
-        
+
+        turn = turn === 'X' ? 'O' : 'X'; // if turn equals X then make it O's turn, else 'X' turn
+        //same in if statment for code above
+        // if (turn === 'X') {
+        // turn = 'O' 
+        // } else {
+        // turn = 'X' 
+        // };
+
+
         console.log(board); // check console logs to make sure it's working!
-        };
+   
+        win = getWinner();
+
+
+        render();
+    };
+
+getWinner = () => { //new way to write a function called arrow function
+    let winner = null;
+    
+    winningCombos.forEach(function(combo, index) {
+        
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+  });
+  //if winner return winner : ELSE IF board includes a space then return null(no winner) : ELSE no winner and no empty spaces thats a Tie 'T'
+  return winner ? winner : board.includes('') ? null : 'T';
+};
+
+    // getWinner = () => {
+    //     return 'HelloWOrld';
+        
+    //     }
+
+updateScoreboard = (winner) => { //cant get the scoreboard
+    if (++scoreboard[winner]==3) {
+        alert("Game over! " + winner + " has won three matches"); 	
+    } 
+}
+
